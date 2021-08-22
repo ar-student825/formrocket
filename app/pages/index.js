f0674ac7e3f92698d22601f3b99d9c28566dbd7e
AR <techrebel2723@gmail.com>
2021-08-22 03:22:01 +0000

    dfg

--- app/pages/index.js
+++ app/pages/index.js
@@ -7,7 +7,7 @@ import {CircularProgress} from "@material-ui/core"
 export default function Home() {
   useEffect(()=>{
     var typed = new Typed('#type', {
-      strings: ["Your Way", "The Right Way", "The Easy Way", "The Programmatic Way", "The Custom Way"],
+      strings: ["Your Way", "The Right Way", "The Easy Way", "The Programmatic Way", "The Custom Way", "The Serverless Way"],
       typeSpeed: 40,
       smartBackspace: true,
       backDelay: 1000,
@@ -32,11 +32,14 @@ export default function Home() {
           <CircularProgress style={{color: 'white'}} />
         </div>
         <h1 className={styles.title}>
+          <noscript>
+            No, I mean it, enable JS, what is that:
+          </noscript>
           Forms, <span id="type"></span> 🚀
         </h1>
 
         <p className={styles.description}>
-        A new way to collect data from HTML forms, and fetch them from our API.<br />Just <a href="https://copy.ar-dev.cf/?content=%3Cform%20method%3D%22POST%22%20id%3D%22formID%22%20action%3D%22https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fpost%22%3E%3Cinput%20name%3D%22myinput%22%20placeholder%3D%22My%20Cool%20Input%22%20%2F%3E%3C%2Fform%3E%0A%3Cscript%3E%0A%20%20%20fetch%28%27https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fscript%27%29.then%28res%20%3D%3E%20res.text%28%29%29.then%28%28%29%3D%3E%7Beval%28res%29%7D%29%0A%3C%2Fscript%3E">make your form POST to us</a>, and we handle the rest.
+        A new way to collect data from HTML forms, and fetch responses from our API.<br />Just <a href="https://copy.ar-dev.cf/?content=%3Cform%20method%3D%22POST%22%20id%3D%22formID%22%20action%3D%22https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fpost%22%3E%3Cinput%20name%3D%22myinput%22%20placeholder%3D%22My%20Cool%20Input%22%20%2F%3E%3C%2Fform%3E%0A%3Cscript%3E%0A%20%20%20fetch%28%27https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fscript%27%29.then%28res%20%3D%3E%20res.text%28%29%29.then%28%28%29%3D%3E%7Beval%28res%29%7D%29%0A%3C%2Fscript%3E">make your form POST to us</a>, and we handle the rest.
         </p>
 
 
