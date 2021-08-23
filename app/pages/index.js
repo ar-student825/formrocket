fa051675d5d1b3839bb0d45f4e83b4b24d0abb51
AR <techrebel2723@gmail.com>
2021-08-23 05:35:59 +0000

    ;

--- app/pages/index.js
+++ app/pages/index.js
@@ -10,7 +10,14 @@ import LogIn from '@material-ui/icons/VpnKey';
 import PreView from '@material-ui/icons/Visibility';
 import LogOut from '@material-ui/icons/ExitToApp';
 import NewForm from '@material-ui/icons/AddCircle';
+import { makeStyles } from '@material-ui/core/styles';
+const Styles = makeStyles((theme) => ({
+  white: {
+    color: "white"
+  }
+}));
 export default function Home() {
+  const styles = Styles()
   useEffect(()=>{
     setTimeout(()=>{
     if (document.getElementById('imagecontainer')) {
@@ -56,13 +63,13 @@ setTimeout(() => {
         A new way to collect data from HTML forms, and fetch responses from our API.<br />Just <a href="https://copy.ar-dev.cf/?content=%3Cform%20method%3D%22POST%22%20id%3D%22formID%22%20action%3D%22https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fpost%22%3E%3Cinput%20name%3D%22myinput%22%20placeholder%3D%22My%20Cool%20Input%22%20%2F%3E%3C%2Fform%3E%0A%3Cscript%3E%0A%20%20%20fetch%28%27https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fscript%27%29.then%28res%20%3D%3E%20res.text%28%29%29.then%28%28%29%3D%3E%7Beval%28res%29%7D%29%0A%3C%2Fscript%3E">make your form POST to us</a>, and we handle the rest. No ads, you control it all.
         </p>        <div className={styles.btns}>
                   <Tooltip title="Login with GitHub">
-                    <IconButton onClick={() => signIn("github")} id="authMain" style={{display: 'none'}}>
-                        <LogIn />
+                    <IconButton className={styles.white} onClick={() => signIn("github")} id="authMain" style={{display: 'none'}}>
+                        <LogIn className={styles.white} />
                     </IconButton>
                     </Tooltip>
                     <Tooltip title="View demo">
-                    <IconButton>
-                        <PreView />
+                    <IconButton className={styles.white}>
+                        <PreView className={styles.white} />
                     </IconButton>
                     </Tooltip>
                     </div>
@@ -87,13 +94,13 @@ setTimeout(() => {
                      <p><b>Oof, </b> there aren&apos;t forms here yet. [Coming soon]</p>
                      <div className={styles.btns}>
                      <Tooltip title="Create new form">
-                    <IconButton styles={{color: 'white'}}>
-                        <NewForm />
+                    <IconButton className={styles.white}>
+                        <NewForm className={styles.white} />
                     </IconButton>
                     </Tooltip>
 
                         <Tooltip title="Logout">
-                    <IconButton onClick={signOut} styles={{color: 'white'}}>
+                    <IconButton onClick={signOut} className={styles.white}>
                         <LogIn />
                     </IconButton>
                     </Tooltip>
