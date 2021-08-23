c2c1ce4a5587e74f72415a5de8f471b85c55f459
AR <techrebel2723@gmail.com>
2021-08-23 00:07:58 +0000

    .

--- app/pages/index.js
+++ app/pages/index.js
@@ -8,7 +8,6 @@ import { useSession, signIn, signOut } from "next-auth/client";
 export default function Home() {
 
   useEffect(()=>{
-    // Bypass the Next.js "<Image>" requirement
     if (document.getElementById('imagecontainer')) {
       var typed = new Typed('#type', {
         strings: ["Your Way", "The Right Way", "The Easy Way", "The Programmatic Way", "The Custom Way", "The Serverless Way"],
@@ -21,7 +20,7 @@ export default function Home() {
     } else return
   },[])
   const [session, loading] = useSession();
-    if (loading) return null
+    if (loading) return (<> <CircularProgress style={{color: 'white'}} /> </>)
     if (!loading && !session) {
   return (
     <div className={styles.container}>
@@ -47,7 +46,7 @@ export default function Home() {
         A new way to collect data from HTML forms, and fetch responses from our API.<br />Just <a href="https://copy.ar-dev.cf/?content=%3Cform%20method%3D%22POST%22%20id%3D%22formID%22%20action%3D%22https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fpost%22%3E%3Cinput%20name%3D%22myinput%22%20placeholder%3D%22My%20Cool%20Input%22%20%2F%3E%3C%2Fform%3E%0A%3Cscript%3E%0A%20%20%20fetch%28%27https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fscript%27%29.then%28res%20%3D%3E%20res.text%28%29%29.then%28%28%29%3D%3E%7Beval%28res%29%7D%29%0A%3C%2Fscript%3E">make your form POST to us</a>, and we handle the rest. No ads, you control it all.
         </p>
                     <button onClick={() => signIn("github")}>
-                        Sign in with Github
+                        Sign in with GitHub
                     </button>
 
       </main>
