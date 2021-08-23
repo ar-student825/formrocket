66a1dd128802283759cb25a8f48bfe92be14abbf
AR <techrebel2723@gmail.com>
2021-08-23 02:42:00 +0000

    ;

--- app/pages/index.js
+++ app/pages/index.js
@@ -71,7 +71,7 @@ setTimeout(() => {
                      <div id="fetchImageUrl" style={{display: 'none'}}>
                         {session.user.image}
                      </div>
-                     <p><b>Oof, </b> there isn&apos;t forms here yet. [Coming soon]</p>
+                     <p><b>Oof, </b> there aren&apos;t forms here yet. [Coming soon]</p>
                   <button onClick={signOut}>Logout</button> <br />
                   </main>
     </>
