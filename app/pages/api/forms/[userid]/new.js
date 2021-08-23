7247dd93a0bd922ed62263e2efeeade10d1ea3d4
AR <techrebel2723@gmail.com>
2021-08-23 17:49:51 +0000

    ;

---
+++ app/pages/api/forms/[userid]/new.js
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router'
+export default function handler(req, res) {
+    const router = useRouter()
+    const reqUserId = router.query.userid
+    if (req.method != 'POST') {
+      res.status(400).json({ error: {code: "INVALID_METHOD", data: "Expected method POST, got " + req.method + "."} })
+    } else if (!req.body || !req.body.formName || typeof req.body.formName == 'string') {
+      res.json({ error: {code: "EMPTY_REQUEST", data: "Expected formName, got null or invalid type."}})
+    } else {
+      res.status(200).json({
+        success: true,
+        data: {
+          form: {
+            name: req.body.formName,
+            ownerId: reqUserId 
+          }
+        }
+      })
+    }
+  }
+    
