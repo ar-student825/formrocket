9b694851d2e67c2017c3ad349db0238af903adf2
AR <techrebel2723@gmail.com>
2021-08-23 19:09:25 +0000

    ;

--- app/pages/api/forms/[userid]/new.js
+++ app/pages/api/forms/[userid]/new.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
     const reqUserId = req.query.userid
     if (req.method != 'POST') {
       res.status(400).json({ error: {code: "INVALID_METHOD", data: "Expected method POST, got " + req.method + "."} })
-    } else if (!req.body || !req.body.formName || typeof req.body.formName == 'string') {
+    } else if (!req.body || !req.body.formName || typeof req.body.formName != 'string') {
       res.json({ error: {code: "EMPTY_REQUEST", data: "Expected formName, got null or invalid type."}})
     } else {
       res.status(200).json({
