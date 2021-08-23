e62ade0a4274eddce53903aed7a87deb94325a64
AR <techrebel2723@gmail.com>
2021-08-23 21:28:35 +0000

    ;

--- app/pages/api/forms/[userid]/[formname]/[formsecret]/post.js
+++ app/pages/api/forms/[userid]/[formname]/[formsecret]/post.js
@@ -1,4 +1,5 @@
-
+const { Database } = require("quickmongo");
+const db = new Database(process.env.MONGODB);
 export default function handler(req, res) {
   if (req.method != 'POST') {
     res.status(400).json({ error: {code: "INVALID_METHOD", data: "Expected method POST, got " + req.method + "."} })
@@ -6,6 +7,8 @@ export default function handler(req, res) {
     res.json({ error: {code: "INVALID_REQUEST", data: "Expected FormDataObject, got null or invalid type."}})
   } else if (!parseInt(req.query.formsecret)) {
     res.status(400).json({ error: {code: "INVALID_CREDENTIALS", data: "Expected valid formSecret, got invalid."} })
+  } else if (!await db.get('users.' + req.query.userid)) {
+    res.status(404).json({ error: {code: "INVALID_CREDENTIALS", data: "Expected valid user, got invalid"}})
   } else {
     res.status(200).json({
       success: true,
