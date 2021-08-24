66d69743259995e3339df9da8e5de3d4d8d73e17
AR <techrebel2723@gmail.com>
2021-08-24 00:05:09 +0000

    ;

--- app/pages/api/forms/[userid]/[formname]/[formsecret]/post.js
+++ app/pages/api/forms/[userid]/[formname]/[formsecret]/post.js
@@ -1,6 +1,5 @@
 const { Database } = require("quickmongo");
-const db = new Database(process.env.MONGODB);
-
+const db = new Database(process.env.MONGODB)
 export default function handler(req, res) {
   if (req.method != 'POST') {
     res.status(400).json({ error: {code: "INVALID_METHOD", data: "Expected method POST, got " + req.method + "."} })
