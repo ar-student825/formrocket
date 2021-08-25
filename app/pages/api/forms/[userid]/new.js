ecb01f9cf6e71583d75755f6d633b102fbd977af
AR <techrebel2723@gmail.com>
2021-08-25 03:05:23 +0000

    ;

--- app/pages/api/forms/[userid]/new.js
+++ app/pages/api/forms/[userid]/new.js
@@ -24,7 +24,7 @@ export default function handler(req, res) {
                 all: []
             }
           }).then(obj => {
-            db.set('users.'+req.query.userid, obj).then(i => {
+            db.set('users', obj).then(i => {
             db.set('users.' + req.query.userid + '.forms.total', users[req.query.userid].forms.total + 1).then(o => {
             res.status(200).json(o)
             })
