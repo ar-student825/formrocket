88f390849a023beee3c51b4b5e954a9b8691cfdd
AR <techrebel2723@gmail.com>
2021-08-26 20:58:28 +0000

    ;

--- app/public/files/onDashLoad.js
+++ app/public/files/onDashLoad.js
@@ -1,3 +1,4 @@
 // ! onDashLoad - 
 
-document ? document.getElementById('avatarContainer').innerHTML = `<img src="${document.getElementById('fetchImageUrl').innerHTML}" width="100px" height="100px" style="border-radius: 50px !important" />` : null
+document.getElementById('avatarContainer').innerHTML = `<img src="${document.getElementById('fetchImageUrl').innerHTML}" width="100px" height="100px" style="border-radius: 50px !important" />`
+console.log('Script loaded - onDashLoad')
