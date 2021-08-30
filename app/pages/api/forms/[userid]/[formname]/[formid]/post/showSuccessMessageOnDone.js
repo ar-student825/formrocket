667a08801515070d7d84ad70e3828229bab35bba
AR <74219764+AR-Student824@users.noreply.github.com>
2021-08-30 14:05:32 -0700

    ;

--- app/pages/api/forms/[userid]/[formname]/[formid]/post/showSuccessMessageOnDone.js
+++ app/pages/api/forms/[userid]/[formname]/[formid]/post/showSuccessMessageOnDone.js
@@ -36,13 +36,20 @@ export default async function handler(req, res) {
         res.status(401).json({ error: {code: "INVALID_FORM", data: "Expected valid formId and / or formName, got invalid"}})
       }
       else {
-          try {
-            res.redirect(`https://ondone.formrocket.me/success${req.query.message != undefined ? "/?message=" + req.query.message : ""}`)
-          } catch {
-              res.json({warning: {code: "SUBMITTED_AND_REDIRECT_FAILED", data: "Expected to redirect to url after submitting, failed to redirect, but submitted form successfully"}})
-          }
-  }
+        var x = users[req.query.userid].forms.all
+        try {
+        x.filter(i => i.formId == req.query.formid)[0].forms.all.push(req.body)
+        } finally {
+          db.set(`users.${req.query.userid}.forms.all`, x).then(() => {
+            try {
+              res.redirect(`https://ondone.formrocket.me/success${req.query.message != undefined ? "/?message=" + req.query.message : ""}`)
+            } catch {
+                res.json({warning: {code: "SUBMITTED_AND_REDIRECT_FAILED", data: "Expected to redirect to url after submitting, failed to redirect, but submitted form successfully"}})
+            }
+          })
+        }        
+      }
   })
   }
-}
+  }
   
