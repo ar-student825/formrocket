cafa1efd423a46fe325b07b7c401127b14e1bb28
AR <74219764+AR-Student824@users.noreply.github.com>
2021-09-02 21:36:46 +0000

    ;

--- app/pages/api/forms/adduser/index.js
+++ app/pages/api/forms/adduser/index.js
@@ -1,5 +1,4 @@
 const { Database } = require("quickmongo");
-import { getSession } from 'next-auth/client'
 import Cors from 'cors'
 const db = new Database(`mongodb+srv://arcodez:${process.env.MONGODB}@cluster0.06v7y.mongodb.net/formrocket?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -26,7 +25,6 @@ function runMiddleware(req, res, fn) {
 }
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors)
-  const session = await getSession({ req })
   if (req.method != 'GET') {
     res.status(400).json({ error: {code: "INVALID_METHOD", data: "Expected method GET, got " + req.method + "."} })
   } else {
