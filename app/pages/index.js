801138fcd3df1893dcb50610dcae98f9f28648c4
AR <74219764+AR-Student824@users.noreply.github.com>
2021-09-04 04:13:09 +0000

    ;

--- app/pages/index.js
+++ app/pages/index.js
@@ -80,14 +80,16 @@ setTimeout(() => {
         </h1>
 
         <p className={styles.description} id="descMain" style={{display: 'none'}}>
-        A new way to collect data from HTML forms, and fetch responses from our API.<br />Just <a href="https://copy.ar-dev.cf/?content=%3Cform%20method%3D%22POST%22%20id%3D%22formID%22%20action%3D%22https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fpost%22%3E%3Cinput%20name%3D%22myinput%22%20placeholder%3D%22My%20Cool%20Input%22%20%2F%3E%3C%2Fform%3E%0A%3Cscript%3E%0A%20%20%20fetch%28%27https%3A%2F%2Fformrocket.ar-dev.cf%2Fapi%2Fforms%2F%3Ausername%2F%3AformName%2F%3AformID%2Fscript%27%29.then%28res%20%3D%3E%20res.text%28%29%29.then%28%28%29%3D%3E%7Beval%28res%29%7D%29%0A%3C%2Fscript%3E">make your form POST to us</a>, and we handle the rest. Everything is powered by our API, so you control it all.
+        A new way to collect data from HTML forms, and fetch responses from our API.<br />Just POST to us, and we handle the rest. Everything is powered by our API, so you control it all.
         </p>        <div className={styles.btns} id="btnsMain" style={{display: 'none'}}>
                   <Tooltip title="Login with GitHub">
                     <IconButton className={s.white} onClick={() => signIn("github")} id="authMain">
                         <LogIn className={s.white} />
                     </IconButton>
                     </Tooltip>
-                    <Tooltip title="View demo">
+                    <Tooltip title="View demo" onClick={() => {
+                      window.location.href = 'https://ar-dev.cf/to/frd'
+                    }}>
                     <IconButton className={s.white}>
                         <PreView className={s.white} />
                     </IconButton>
